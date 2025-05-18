5a1cca1afde62c81c7feb161341f21686d5743dc
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-19 05:00:46 +0800

    Controller层实现跳转至index页面的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/MappingController.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/MappingController.java
@@ -1,15 +1,19 @@
 package com.samgoldsee.movie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
+@Tag(name = "MappingController")
 public class MappingController {
 
-    @RequestMapping("/")
+    @Operation(summary = "跳转至index请求")
+    @GetMapping("/")
     public String index() {
-        return "hello";
+        return "index";
     }
 }
