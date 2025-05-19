2b20f862ecbf7430c3fb049d28d1c5e2aa4a1786
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-20 00:47:44 +0800

    Common层实现邮件业务异常类

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/exception/EmailException.java
@@ -0,0 +1,10 @@
+package com.samgoldsee.movie.exception;
+
+/**
+ * 邮件异常类
+ */
+public class EmailException extends BusinessException {
+    public EmailException(String message) {
+        super(message);
+    }
+}
