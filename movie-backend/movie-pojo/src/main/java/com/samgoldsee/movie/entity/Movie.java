d6e3fa39108579d5a9589df84b5f5edcd47eb667
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-19 09:12:53 +0800

    Pojo层实现电影对象实体类

---
+++ movie-backend/movie-pojo/src/main/java/com/samgoldsee/movie/entity/Movie.java
@@ -0,0 +1,51 @@
+package com.samgoldsee.movie.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 电影id
+    private Integer id;
+
+    // 电影名
+    private String name;
+
+    // 简介
+    private String description;
+
+    // 电影地区id
+    private Integer zoneId;
+
+    // 浏览数
+    private Integer view;
+
+    // 海报链接
+    private String poster;
+
+    // 观看权限(0:普通 1:VIP)
+    private Boolean permission;
+
+    // 上线日期
+    private Date releaseDate;
+
+    // 创建时间
+    private LocalDateTime createTime;
+
+    // 更新时间
+    private LocalDateTime updateTime;
+}
