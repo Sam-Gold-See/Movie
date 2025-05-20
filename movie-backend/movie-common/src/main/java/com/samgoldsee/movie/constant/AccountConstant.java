5438258a37dbfc9efd3bb00a96ff4d96eba2695b
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-20 10:21:59 +0800

    Server层完善账号相关信息常量类类：验证码字符数组、Redis存储键

--- movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/AccountConstant.java
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/AccountConstant.java
@@ -7,4 +7,10 @@ public class AccountConstant {
 
     // 验证码有效时长（分钟）
     public static final Integer VERIFICATION_CODE_TTL = 5;
+
+    // 验证码字符数组
+    public static final char[] VERIFICATION_CODE_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    // Redis 存储验证码键常量
+    public static final String REDIS_KEY = "code:list:";
 }
