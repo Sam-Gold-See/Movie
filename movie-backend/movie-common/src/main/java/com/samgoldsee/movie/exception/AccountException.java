c9419c074109055d1849db035232c50f985f3ea0
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-20 15:20:41 +0800

    Common层实现账号业务异常类

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/exception/AccountException.java
@@ -0,0 +1,7 @@
+package com.samgoldsee.movie.exception;
+
+public class AccountException extends BusinessException {
+    public AccountException(String message) {
+        super(message);
+    }
+}
