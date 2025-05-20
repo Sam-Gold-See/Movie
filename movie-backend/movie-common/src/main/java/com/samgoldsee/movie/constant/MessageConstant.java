d83cb084526c41fed50e17dac5b22acd1e57f45e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-20 15:17:40 +0800

    Common层完善信息提示常量类：账号已存在、未知错误

--- movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/MessageConstant.java
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/MessageConstant.java
@@ -7,4 +7,6 @@ public class MessageConstant {
     public static final String SEND_EMAIL_FAIL = "发送邮件失败";
     public static final String EMAIL_REPEAT = "请求过于频繁，请稍后再试";
     public static final String VERIFICATION_CODE_ERROR = "验证码错误";
+    public static final String ACCOUNT_ALREADY_EXIST = "账号已存在";
+    public static final String UNKNOWN_ERROR = "未知错误";
 }
