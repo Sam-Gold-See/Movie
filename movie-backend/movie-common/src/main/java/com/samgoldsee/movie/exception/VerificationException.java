501ba91a602f0a9936c60b7ebd6075c4733687f0
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-20 12:56:48 +0800

    Common层实现验证业务异常类

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/exception/VerificationException.java
@@ -0,0 +1,7 @@
+package com.samgoldsee.movie.exception;
+
+public class VerificationException extends BusinessException {
+    public VerificationException(String message) {
+        super(message);
+    }
+}
