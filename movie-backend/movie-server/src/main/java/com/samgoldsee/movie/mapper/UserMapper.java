7e63a1625b53b3a38ec791776900202258b967a9
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-21 10:49:24 +0800

    Mapper层实现新增用户的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/UserMapper.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/UserMapper.java
@@ -1,6 +1,9 @@
 package com.samgoldsee.movie.mapper;
 
+import com.samgoldsee.movie.annotation.AutoFill;
 import com.samgoldsee.movie.entity.User;
+import com.samgoldsee.movie.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +18,15 @@ public interface UserMapper {
      */
     @Select("SELECT * FROM `user` WHERE `email` = #{email}")
     User getByEmail(String email);
+
+    /**
+     * 新增用户记录
+     *
+     * @param user 用户对象实体类
+     */
+    @AutoFill(operation = OperationType.INSERT)
+    @Insert("INSERT INTO `user` " +
+            "(`name`, `email`, `password`, `gender`, `type`, `avatar`, `create_time`, `update_time`) " +
+            "VALUES (#{name}, #{email}, #{password}, #{gender}, #{type}, #{avatar}, #{createTime}, #{updateTime})")
+    void insert(User user);
 }
