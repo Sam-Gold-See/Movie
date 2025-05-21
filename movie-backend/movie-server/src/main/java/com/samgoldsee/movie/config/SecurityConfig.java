b961ea766b241f28179694c2b659b22119ba059b
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-21 14:41:16 +0800

    Server层配置Spring Security同时注入BCrypt密码加密器容器

---
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.samgoldsee.movie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 配置放行路径
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+
+                                // 静态资源
+                                "/css/**", "/img/**", "/js/**", "/scss/**", "/vendor/**",
+
+                                // 页面请求
+                                "/", "/toLogin", "/toRegister", "/email/sendCode", "/email/checkCode", "/user/register", "/user/login",
+
+                                // knife4j接口文档路径
+                                "/doc.html", "swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**")
+
+                        .permitAll().anyRequest().authenticated())
+
+                // 配置登录相关属性
+                .formLogin(form -> form
+                        .loginPage("/toLogin")
+                        .loginProcessingUrl("/user/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+
+                // 配置退出相关属性
+                .logout(logout -> logout
+                        .logoutUrl("/toLogout")
+                        .logoutSuccessUrl("/toLogin"))
+
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
