9d24025cae254b30b7ddd76360dcc7abd38242c0
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-21 13:31:52 +0800

    Pojo层实现用户登录DTO对象实体类

---
+++ movie-backend/movie-pojo/src/main/java/com/samgoldsee/movie/dto/UserLoginDTO.java
@@ -0,0 +1,29 @@
+package com.samgoldsee.movie.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "用户登录DTO", title = "UserLoginDTO")
+public class UserLoginDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 用户邮箱地址
+    @Schema(description = "邮箱地址", example = "user@example.com", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String email;
+
+    // 用户登录密码
+    @Schema(description = "登录密码", example = "123456", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String password;
+}
