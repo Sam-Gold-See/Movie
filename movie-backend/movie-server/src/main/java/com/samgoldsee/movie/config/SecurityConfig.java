8baacddb7a4bdd1ef7ac0ed1a58570d496a24b88
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-22 09:29:02 +0800

    Server层配置Spring Security开放权限接口

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/config/SecurityConfig.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                                 "/css/**", "/img/**", "/js/**", "/scss/**", "/vendor/**",
 
                                 // 页面请求
-                                "/", "/toLogin", "/toRegister", "/email/sendCode", "/email/checkCode", "/user/register", "/user/login",
+                                "/toLogin", "/toRegister", "/toForgotPassword", "/email/sendCode", "/email/checkCode", "/user/register", "/user/login",
 
                                 // knife4j接口文档路径
                                 "/doc.html", "swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**")
