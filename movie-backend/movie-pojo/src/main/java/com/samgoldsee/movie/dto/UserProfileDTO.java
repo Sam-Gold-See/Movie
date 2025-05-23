e591b148fd4f989611e56d71079e097e0fae10f8
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-23 13:11:12 +0800

    Pojo层实现用户信息DTO对象

---
+++ movie-backend/movie-pojo/src/main/java/com/samgoldsee/movie/dto/UserProfileDTO.java
@@ -0,0 +1,41 @@
+package com.samgoldsee.movie.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(title = "UserProfileDTO", description = "用户信息DTO")
+public class UserProfileDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // 用户名
+    @Schema(description = "用户名", example = "SamGoldSee", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    // 用户邮箱地址
+    @Schema(description = "邮箱地址", example = "user@example.com", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String email;
+
+    // 用户登录密码
+    @Schema(description = "登录密码", example = "123456", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String password;
+
+    // 用户性别(M:男 F:女)
+    @Schema(description = "性别(M:男 F:女)", example = "M", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Character gender;
+
+    // 头像资源链接
+    @Schema(description = "头像资源链接", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String avatar;
+}
