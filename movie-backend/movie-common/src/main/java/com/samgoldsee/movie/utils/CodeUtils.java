69fe5bd7fdb113c5be1848f7cd44ee3d78f5f531
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-23 14:51:25 +0800

    Common层实现数码工具类用于大写化用户验证码来校对

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/utils/CodeUtils.java
@@ -0,0 +1,25 @@
+package com.samgoldsee.movie.utils;
+
+/**
+ * 数码相关工具
+ */
+public class CodeUtils {
+
+    /**
+     * 将数码中所有字母大写
+     *
+     * @param code 原码
+     * @return String类目标码
+     */
+    public static String upperLetters(String code) {
+        StringBuilder result = new StringBuilder();
+        for (char c : code.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(Character.toUpperCase(c));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
