dd50fa1f2939be9977e5252c15dad365513834fd
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-26 17:19:34 +0800

    Common层实现阿里支付信息常量类

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/AliPayConstant.java
@@ -0,0 +1,16 @@
+package com.samgoldsee.movie.constant;
+
+/**
+ * 阿里支付信息常量类
+ */
+public class AliPayConstant {
+
+    // 网关
+    public static final String GATEWAY_URL = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
+
+    // 返回格式
+    public static final String FORMAT = "JSON";
+
+    // 字体编码
+    public static final String CHARSET = "UTF-8";
+}
