0576af5bbb92243d3e1063ebdb3888cc8453503d
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-26 11:44:11 +0800

    Common层实现OSS信息常量类

---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/constant/OSSConstant.java
@@ -0,0 +1,19 @@
+package com.samgoldsee.movie.constant;
+
+/**
+ * OSS信息常量类
+ */
+public class OSSConstant {
+
+    // Bucket中用户文件路径
+    public static final String USER_PATH = "user/";
+
+    // Bucket中电影文件路径
+    public static final String MOVIE_PATH = "movie/";
+
+    // Bucket中导演文件路径
+    public static final String DIRECTOR_PATH = "director/";
+
+    // Bucket中演员文件路径
+    public static final String ACTOR_PATH = "actor/";
+}
