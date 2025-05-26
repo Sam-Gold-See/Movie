fd0ec8ddde465b0ae18ff70715c668d442278bf3
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-26 17:23:13 +0800

    Common层完善阿里支付异常类

--- movie-backend/movie-common/src/main/java/com/samgoldsee/movie/exception/AliPayApiException.java
+++
@@ -1,7 +0,0 @@
-package com.samgoldsee.movie.exception;
-
-public class AliPayApiException extends BusinessException {
-    public AliPayApiException(String message) {
-        super(message);
-    }
-}
---
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/exception/AliPayException.java
@@ -0,0 +1,7 @@
+package com.samgoldsee.movie.exception;
+
+public class AliPayException extends BusinessException {
+    public AliPayException(String message) {
+        super(message);
+    }
+}
