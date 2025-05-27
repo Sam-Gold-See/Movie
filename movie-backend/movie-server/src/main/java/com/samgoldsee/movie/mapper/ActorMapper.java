0ed8debeb2e4fc093f560875a272f1ec0c696d5f
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-27 18:32:44 +0800

    Mapper层实现根据电影ID查询演员信息的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/ActorMapper.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/ActorMapper.java
@@ -14,4 +14,14 @@ public interface ActorMapper {
      */
     @Select("SELECT * FROM `actor`")
     List<Actor> selectAll();
+
+    /**
+     * 根据电影ID查询演员信息
+     *
+     * @param movieId 电影ID
+     */
+    @Select("SELECT * FROM actor a " +
+            "JOIN `actor-movie` am ON a.id = am.actor_id " +
+            "WHERE movie_id = #{movieId}")
+    Actor getByMovieId(Integer movieId);
 }
