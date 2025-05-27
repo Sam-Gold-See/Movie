228cb5d003c0f79a17f358c724cd0d196c496869
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-27 11:18:21 +0800

    Common层完善多数据查询响应对象的接口文档展示

--- movie-backend/movie-common/src/main/java/com/samgoldsee/movie/result/PageResult.java
+++ movie-backend/movie-common/src/main/java/com/samgoldsee/movie/result/PageResult.java
@@ -1,11 +1,18 @@
 package com.samgoldsee.movie.result;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.List;
 
+@Data
+@Schema(description = "多数据查询响应对象", title = "PageResult")
 public class PageResult<T> implements Serializable {
 
+    @Schema(description = "总记录数", example = "5")
     private long total;
 
+    @Schema(description = "记录数据")
     private List<T> records;
 }
