a7018fd2bbd98a757e535af8a1dc058a1ec8fac6
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-28 11:27:58 +0800

    Mapper层实现查询全部电影地区的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/MovieMapper.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/MovieMapper.java
@@ -2,6 +2,7 @@ package com.samgoldsee.movie.mapper;
 
 import com.samgoldsee.movie.entity.Movie;
 import com.samgoldsee.movie.entity.MovieType;
+import com.samgoldsee.movie.entity.MovieZone;
 import com.samgoldsee.movie.vo.MovieVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -30,4 +31,10 @@ public interface MovieMapper {
      */
     @Select("SELECT * FROM `movie_type` ORDER BY `id`")
     List<MovieType> selectType();
+
+    /**
+     * 查询电影地区分类
+     */
+    @Select("SELECT * FROM `movie_zone` ORDER BY `id`")
+    List<MovieZone> selectZone();
 }
