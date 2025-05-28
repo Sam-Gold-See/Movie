5014e41960c5a1022c5592b2562259cf83e72ae7
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-28 16:04:59 +0800

    Controller层实现获取查询电影本周播放排行请求的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/RecordController.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/RecordController.java
@@ -59,6 +59,22 @@ public class RecordController {
         return Result.success(result);
     }
 
+    /**
+     * 查询本周播放情况
+     */
+    @GetMapping("/rankWeek")
+    @Operation(summary = "查询本周播放排行")
+    @ApiResponse(responseCode = "200", description = "成功", content = @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = MovieRankVOPageResultResult.class)
+    ))
+    public Result<PageResult<MovieRankVO>> rankWeek(){
+        UserSession userSession = (UserSession) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("用户(id:{})查询本周电影播放排行", userSession.getId());
+        PageResult<MovieRankVO> result = recordService.rankWeek();
+        return Result.success(result);
+    }
+
     @Schema(name = "Result<PageResult<MovieRankVO>>", description = "包含MovieRankVO的多数据统一响应对象")
     private static class MovieRankVOPageResultResult extends Result<PageResult<MovieRankVO>> {
     }
