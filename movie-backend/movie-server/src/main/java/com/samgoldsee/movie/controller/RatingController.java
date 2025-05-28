c234b17ae6c4a7c5e50117f6ba8884627cb54ce2
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-28 14:56:00 +0800

    Controller层实现获取查询电影的好评排行请求的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/RatingController.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/controller/RatingController.java
@@ -1,8 +1,10 @@
 package com.samgoldsee.movie.controller;
 
 import com.samgoldsee.movie.dto.UserSession;
+import com.samgoldsee.movie.result.PageResult;
 import com.samgoldsee.movie.result.Result;
 import com.samgoldsee.movie.service.RatingService;
+import com.samgoldsee.movie.vo.MovieRankVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,6 +14,7 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +35,7 @@ public class RatingController {
      * @param rating  评分
      */
     @PostMapping("/upload")
-    @Operation(description = "用户评分")
+    @Operation(summary = "用户评分")
     @ApiResponse(responseCode = "200", description = "成功", content = @Content(
             mediaType = MediaType.APPLICATION_JSON_VALUE,
             schema = @Schema(implementation = Result.class)
@@ -42,5 +45,25 @@ public class RatingController {
         log.info("用户(id:{})给电影(id:{})评分:{}", userSession.getId(), movieId, rating);
         ratingService.upload(userSession.getId(), movieId, rating);
         return Result.success();
+    }
+
+    /**
+     * 查询好评排行
+     */
+    @GetMapping("/rank")
+    @Operation(summary = "查询好评排行")
+    @ApiResponse(responseCode = "200", description = "成功", content = @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = MovieRankVOPageResultResult.class)
+    ))
+    public Result<PageResult<MovieRankVO>> rank() {
+        UserSession userSession = (UserSession) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("用户(id:{})查询电影好评排行", userSession.getId());
+        PageResult<MovieRankVO> result = ratingService.rank();
+        return Result.success(result);
+    }
+
+    @Schema(name = "Result<PageResult<MovieRankVO>>", description = "包含MovieRankVO的多数据统一响应对象")
+    private static class MovieRankVOPageResultResult extends Result<PageResult<MovieRankVO>> {
     }
 }
