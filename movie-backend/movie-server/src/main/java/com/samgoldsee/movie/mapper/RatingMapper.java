a4456edae7abe702c41acf7259bdd7a57fc15547
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-28 10:23:56 +0800

    Mapper层实现插入用户影评的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/RatingMapper.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/RatingMapper.java
@@ -1,6 +1,9 @@
 package com.samgoldsee.movie.mapper;
 
+import com.samgoldsee.movie.annotation.AutoFill;
 import com.samgoldsee.movie.entity.Rating;
+import com.samgoldsee.movie.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +18,13 @@ public interface RatingMapper {
      */
     @Select("SELECT * FROM `rating` WHERE `user_id` = #{userID} AND `movie_id` = #{movieId} ")
     Rating selectByUMId(Integer userId, Integer movieId);
+
+    /**
+     * 插入用户影评
+     *
+     * @param ratingOb 评分实体对象
+     */
+    @AutoFill(operation = OperationType.INSERT)
+    @Insert("INSERT INTO `rating` (`rating`, `user_id`, `movie_id`, `create_time`, `update_time`) VALUES (#{rating}, #{userId}, #{movieId}, #{createTime}, #{updateTime})")
+    void insert(Rating ratingOb);
 }
