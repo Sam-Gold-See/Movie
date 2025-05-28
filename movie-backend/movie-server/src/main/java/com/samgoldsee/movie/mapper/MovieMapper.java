573aa3562e9ba3d9fc6f92a11725a6d2b749a936
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-05-28 11:16:52 +0800

    Mapper层实现查询全部电影种类的功能

--- movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/MovieMapper.java
+++ movie-backend/movie-server/src/main/java/com/samgoldsee/movie/mapper/MovieMapper.java
@@ -1,6 +1,7 @@
 package com.samgoldsee.movie.mapper;
 
 import com.samgoldsee.movie.entity.Movie;
+import com.samgoldsee.movie.entity.MovieType;
 import com.samgoldsee.movie.vo.MovieVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -23,4 +24,10 @@ public interface MovieMapper {
      */
     @Select("SELECT * FROM movie WHERE id = #{id}")
     Movie selectById(Integer id);
+
+    /**
+     * 查询电影种类分类
+     */
+    @Select("SELECT * FROM `movie_type` ORDER BY `id`")
+    List<MovieType> selectType();
 }
